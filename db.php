d22cc8ed5eff46629b918e31759aec34e6ccb0bd
jls922 <jls922@drexel.edu>
2026-02-04 10:56:08 -0500

    db edited

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <////?php
 // database connection settings for my own phpmyadmin and MAMP ONLY.... once i find the drexel admin thing again ill do it there
 $host = 'localhost';
 $dbname = 'simplx_db';
@@ -12,18 +12,18 @@ try {
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
-?>
+?> -->
 
-<!-- <////?php
+<?php
 $host = 'localhost';  // whatever drexel provides
-$dbname = 'your_drexel_database_name';
-$username = 'your_drexel_username';
-$password = 'your_drexel_password';
+$dbname = 'jls922_db';
+$username = 'jls922';
+$password = '8l1i8Kg0B12ghaZ/'; //same one that you use to log in to phpmyadmin
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage())////;
+    die("Connection failed: " . $e->getMessage());
 }
-?> -->
+?>
